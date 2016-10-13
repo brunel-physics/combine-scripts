2ba33171d5d0c5afa0a09306e14cd8c74a7704c1
Corin <lunik1@users.noreply.github.com>
2016-10-13 21:40:03 +0100

    Update for 2016

---
+++ combine.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+printf "\e[1m!=== OBSERVED SIGNIFICANCE ===!\e[0m\n"
+combine -M ProfileLikelihood --signif --cminDefaultMinimizerType=Minuit2 -s -1 datacard.txt 2> /dev/null
+
+printf "\n\n\e[1m!=== A PRIORI EXPECTED SIGNIFCANCE===!\e[0m\n"
+combine -M ProfileLikelihood --signif --cminDefaultMinimizerType=Minuit2 -t -1 --expectSignal=1.0 -s -1 datacard.txt 2> /dev/null
+
+printf "\n\n\e[1m!=== A POSITERIORI EXPECTED SIGNIFICANCE===!\e[0m\n"
+combine -M ProfileLikelihood --signif --cminDefaultMinimizerType=Minuit2 -t -1 --expectSignal=1.0 -s -1 --toysFreq datacard.txt 2> /dev/null
