1db3929977c71807756a9ca75f43eeffd1ad073b
lunik1 <lunik1@users.noreply.github.com>
2018-09-20 12:34:03 +0100

    Update and use madgraph

--- combine.sh
+++ combine.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm -f higgsCombine* impacts.json impacts.pdf datacard*.root datacard_syst_only.txt
+
 # Create workspaces
 text2workspace.py datacard_ee.txt
 text2workspace.py datacard_mumu.txt
@@ -12,29 +14,31 @@ text2workspace.py datacard_syst_only.txt
 
 printf "\n"
 printf "\e[1m!===IMPACTS===!\e[0m\n"
-combineTool.py -M Impacts -d datacard_syst_only.root -m 125 -t -1 --expectSignal=1.0 --doInitialFit
-combineTool.py -M Impacts -d datacard_syst_only.root -m 125 -t -1 --expectSignal=1.0 --doFits --parallel $(nproc)
-combineTool.py -M Impacts -d datacard_syst_only.root -m 125 -t -1 --expectSignal=1.0 -o impacts.json
+combineTool.py -M Impacts -d datacard_syst_only.root -m 125 --rMin -5 --expectSignal=1.0 --doInitialFit --robustFit=1 --setRobustFitAlgo=Minuit2 --setRobustFitStrategy=1 
+rename '123456.' '' higgsCombine_initialFit_Test*
+combineTool.py -M Impacts -d datacard_syst_only.root -m 125 --rMin -5 --expectSignal=1.0 --doFits --parallel $(nproc) --robustFit=1 --setRobustFitAlgo=Minuit2 --setRobustFitStrategy=1 
+rename '123456.' '' higgsCombine_paramFit_Test*
+combineTool.py -M Impacts -d datacard_syst_only.root -m 125 --rMin -5 --expectSignal=1.0 --robustFit=1 --setRobustFitAlgo=Minuit2 --setRobustFitStrategy=1 -o impacts.json
 plotImpacts.py -i impacts.json -o impacts
 
 printf "\n"
 printf "\e[1m!===EXPECTED SIGNIFICANCES===!\e[0m\n"
 printf "\e[1m!=== ee EXPECTED SIGNIFICANCE===!\e[0m\n"
-combine -M Significance --cminDefaultMinimizerType=Minuit2 -t -1 --expectSignal=1.0 -s -1 datacard_ee.root
+combine -M Significance --cminDefaultMinimizerType=Minuit2 --expectSignal=1.0 -s -1 datacard_ee.root
 
 printf "\n\n\e[1m!=== μμ EXPECTED SIGNIFICANCE===!\e[0m\n"
-combine -M Significance --cminDefaultMinimizerType=Minuit2 -t -1 --expectSignal=1.0 -s -1 datacard_mumu.root
+combine -M Significance --cminDefaultMinimizerType=Minuit2 --expectSignal=1.0 -s -1 datacard_mumu.root
 
 printf "\n\n\e[1m!=== COMBINED EXPECTED SIGNIFICANCE===!\e[0m\n"
-combine -M Significance --cminDefaultMinimizerType=Minuit2 -t -1 --expectSignal=1.0 -s -1 datacard.root
+combine -M Significance --cminDefaultMinimizerType=Minuit2 --expectSignal=1.0 -s -1 datacard.root
 
 printf "\n"
 printf "\e[1m!===SIGNAL STRENGTHS===!\e[0m\n"
 printf "\e[1m!=== ee EXPECTED SIGNAL STRENGTH===!\e[0m\n"
-combine -M FitDiagnostics -t -1 --expectSignal=1.0 -s -1 --rMin -5 datacard_ee.root
+combine -M FitDiagnostics --robustFit=1 --setRobustFitAlgo=Minuit2 --setRobustFitStrategy=1 --expectSignal=1.0 -s -1 --rMin -5 datacard_ee.root
 
 printf "\n\n\e[1m!=== μμ EXPECTED SIGNAL STRENGTH===!\e[0m\n"
-combine -M FitDiagnostics -t -1 --expectSignal=1.0 -s -1 --rMin -5 datacard_mumu.root
+combine -M FitDiagnostics --robustFit=1 --setRobustFitAlgo=Minuit2 --setRobustFitStrategy=1 --expectSignal=1.0 -s -1 --rMin -5 datacard_mumu.root
 
 printf "\n\n\e[1m!=== COMBINED EXPECTED SIGNAL STRENGTH===!\e[0m\n"
-combine -M FitDiagnostics -t -1 --expectSignal=1.0 -s -1 --rMin -5 datacard.root
+combine -M FitDiagnostics --robustFit=1 --setRobustFitAlgo=Minut2 --setRobustFitStrategy=1 --expectSignal=1.0 -s -1 --rMin -5 --saveShapes --saveWithUncertainties datacard.root
