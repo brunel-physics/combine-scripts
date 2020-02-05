aa486790bfb6d5858d871f85edb43162b68b5b4c
lunik1 <lunik1@users.noreply.github.com>
2020-02-05 15:50:32 +0000

    Upate final.sh to create systematic impact plots

--- final.sh
+++ final.sh
@@ -13,7 +13,8 @@ combine () {
 }
 
 
-rm -rf 2016+2017/
+rm -rf 2016+2017/datacards
+rm -rf 2016+2017/shapes
 mkdir -p 2016+2017/datacards 2016+2017/shapes
 
 cp 2016/datacards/datacard_combined.txt 2016+2017/datacards/datacard_combined_2016.txt
@@ -27,4 +28,14 @@ text2workspace.py --poisson=2147483647 --no-optimize-pdfs 2016+2017/datacards/da
 
 combine -M Significance -d 2016+2017/datacards/datacard_all.root --bruteForce --setBruteForceTypeAndAlgo $minlib,$algo --uncapped 1
 
+
 combine -M FitDiagnostics -d 2016+2017/datacards/datacard_all.root --robustFit=1 --setRobustFitAlgo=$minlib,$algo --setRobustFitStrategy=$strategy --rMin -5
+
+# Impacts
+combine -M Impacts -d 2016+2017/datacards/datacard_all.root --rMin -5 --doInitialFit --robustFit=1 --setRobustFitAlgo=$minlib,$algo --setRobustFitStrategy=$strategy --name all > logs/impacts_all.log 2>&1
+rename "${seed}." '' higgsCombine_initialFit_all*
+combine -M Impacts -d 2016+2017/datacards/datacard_all.root --rMin -5 --doFits --parallel $(nproc) --robustFit=1 --setRobustFitAlgo=$minlib,$algo --setRobustFitStrategy=$strategy --name all >> logs/impacts_all.log 2>&1
+rename "${seed}." '' higgsCombine_paramFit_all*
+combine -M Impacts -d 2016+2017/datacards/datacard_all.root --rMin -5 --robustFit=1 --setRobustFitAlgo=$minlib,$algo --setRobustFitStrategy=$strategy -o out/2016+2017/impacts_all.json --name all >> logs/impacts_all.log 2>&1
+python stat_remover.py out/2016+2017/impacts_all.json
+plotImpacts.py -i out/2016+2017/impacts_all.json -o out/2016+2017/impacts_all --translate 2016+2017/json/thesis.json --per-page 50 --label-size 0.03
